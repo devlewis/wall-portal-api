3ea31391841bcdc23d599003413f3c587e27b90d
devlewis <devree.lewis@gmail.com>
2020-06-11 16:10:56 -0400

    first commit

--- src/app.js
+++ src/app.js
@@ -1,20 +1,34 @@
 require("dotenv").config();
+const btoa = require("btoa");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
-
+const fetch = require("node-fetch");
+const user = process.env.API_KEY;
+const pw = process.env.PW;
 const app = express();
+const { CLIENT_ORIGIN } = require("./config");
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: CLIENT_ORIGIN }));
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+app.get("/api", (req, res) => {
+  const url = process.env.URL;
+  const options = {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Basic " + btoa(`${user}:${pw}`),
+    },
+  };
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((responseJson) => res.status(200).json(responseJson));
 });
 
 app.use(function errorHandler(error, req, res, next) {
--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || "development"
+  NODE_ENV: process.env.NODE_ENV || "development",
+  CLIENT_ORIGIN: "*",
+  API_KEY: process.env.API_KEY,
+  PW: process.env.PW,
+  URL: process.env.URL,
 };
